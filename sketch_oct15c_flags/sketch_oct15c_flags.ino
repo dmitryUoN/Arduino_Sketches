ea3e5ae282aaa3d094a6610b450b9c87c7d7799f
Dmitry <nedimon1983@gmail.com>
2022-10-15 21:13:26 +0300

    Update sketch_oct15c_flags.ino
    Добавил фильтр-задержку для устранения дребезга кнопки.

--- sketch_oct15c_flags/sketch_oct15c_flags.ino
+++ sketch_oct15c_flags/sketch_oct15c_flags.ino
@@ -1,6 +1,7 @@
 boolean button_flag = 0;
 boolean button;
 boolean led_enabled = 0;
+unsigned long last_pressed; // время последнего нажатия для устранения дребезга кнопки
 
 void setup() {
   pinMode(3, INPUT_PULLUP); // встроенная подтяжка
@@ -10,11 +11,12 @@ void setup() {
 
 void loop() {
   button = !digitalRead(3); //считываю положение кнопки
-  if (button == 1 && button_flag == 0){
+  if (button == 1 && button_flag == 0 && millis() - last_pressed > 50){ //50 миллисекунд на дребезг
       button_flag = 1;
       Serial.println("Button pressed");
       led_enabled = !led_enabled;
       digitalWrite (13, led_enabled);
+      last_pressed = millis();
   }
   if (button == 0 && button_flag == 1){
       button_flag = 0;
